1d0f02dc1301ec5f42895e01f404631d7a34f0ec
komeil <catinrage@gmail.com>
2025-12-17 12:09:12 +0330

    ci: Added --no-engine to prisma generate so it don't complain about missing env vars, also removed redundant generate command in deploy.sh

--- prisma/deploy.sh
+++ prisma/deploy.sh
@@ -4,10 +4,6 @@
 echo "Running Prisma migrations..."
 bun prisma migrate deploy
 
-# Generate Prisma client
-echo "Generating Prisma client..."
-bun prisma generate
-
 # Seed the database
 echo "Seeding the database..."
 bun prisma db seed
